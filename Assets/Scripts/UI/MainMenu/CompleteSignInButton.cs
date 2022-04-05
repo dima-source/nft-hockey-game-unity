e6e7b7c7e3f53fc1c8ec629955e561a022ca4f1e
yurii <stolbetskiy.yura@gmail.com>
2022-04-05 21:59:29 +0300

    get_available_players

--- Assets/Scripts/UI/MainMenu/CompleteSignInButton.cs
+++ Assets/Scripts/UI/MainMenu/CompleteSignInButton.cs
@@ -1,3 +1,5 @@
+using System.Dynamic;
+using NearClientUnity;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,13 +11,29 @@ namespace UI.MainMenu
 
         [SerializeField] private Text accountId;
         
+        [SerializeField] private Text opponentsText;
+        
+        
         public async void Complete()
         {
             Debug.Log(inputUri.text);
             await NearPersistentManager.Instance.WalletAccount.CompleteSignIn(inputUri.text);
             if(NearPersistentManager.Instance.WalletAccount.IsSignedIn())
             {
-                accountId.text = "Your account id: " + NearPersistentManager.Instance.WalletAccount.GetAccountId(); 
+                ContractNear gameContract = await NearPersistentManager.Instance.GetContract();
+                
+                dynamic args = new ExpandoObject();
+                args.from_index = 0;
+                args.limit = 50;
+
+                var opponents = await gameContract.View("get_available_players", args);
+
+                dynamic r = opponents.result;
+                
+                opponentsText.text = "Your opponents: " + r + "  ...";
+                
+
+                accountId.text = "Your account id: " + NearPersistentManager.Instance.WalletAccount.GetAccountId();
             }   
         }
     }
---
+++ Assets/Scripts/UI/MainMenu/PlayGameButton.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace UI.MainMenu
+{
+    public class PlayGameButton : MonoBehaviour
+    {
+    }
+}
