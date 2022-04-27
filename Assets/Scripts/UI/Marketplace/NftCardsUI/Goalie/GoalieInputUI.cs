d4cc64f9523d127a2bafbc0adab278a7455c6448
yurii <stolbetskiy.yura@gmail.com>
2022-04-28 01:47:01 +0300

    Added minting goalie nft

--- Assets/Scripts/UI/Marketplace/NftCardsUI/Goalie/GoalieInputUI.cs
+++ Assets/Scripts/UI/Marketplace/NftCardsUI/Goalie/GoalieInputUI.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Dynamic;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +8,11 @@ namespace UI.Marketplace.NftCardsUI.Goalie
 {
     public class GoalieInputUI : NftCardInputUI
     {
+        [SerializeField] private Dropdown role;
+        [SerializeField] private Dropdown hand;
+        
+        [SerializeField] private InputField number;
+        
         [SerializeField] private InputField gloveAndBlocker;
         [SerializeField] private InputField pads;
         [SerializeField] private InputField stand;
@@ -14,7 +21,25 @@ namespace UI.Marketplace.NftCardsUI.Goalie
 
         public override void MintCard(Dictionary<string, double> royalties, string url)
         {
-            throw new System.NotImplementedException();
+            dynamic goalie = new ExpandoObject();
+            
+            goalie.type = "GoaliePos";
+            goalie.position = "GoaliePos";
+            goalie.role = role.options[role.value].text;
+            goalie.hand = hand.options[hand.value].text;
+            goalie.number = number.text;
+            goalie.stats = new []
+            { 
+                int.Parse(gloveAndBlocker.text),
+                int.Parse(pads.text),
+                int.Parse(stand.text),
+                int.Parse(stretch.text),
+                int.Parse(morale.text)
+            };
+            
+            string goalieJson = JsonConvert.SerializeObject(goalie);
+            
+            Near.MarketplaceContract.Actions.MintNFT(royalties, url, name.text, goalieJson);
         }
     }
 }
