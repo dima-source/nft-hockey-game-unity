04bed8625948d3a4042bc5ece302a035564e93ab
yurii <stolbetskiy.yura@gmail.com>
2022-05-25 23:47:34 +0300

    Implemented SetYourOwnButton

--- Assets/Scripts/UI/Main menu/UIPopups/UIPopupOpponentsBets.cs
+++ Assets/Scripts/UI/Main menu/UIPopups/UIPopupOpponentsBets.cs
@@ -1,7 +1,14 @@
+using UnityEngine;
+
 namespace UI.Main_menu.UIPopups
 {
     public class UIPopupOpponentsBets : UIPopup
     {
-        
+        [SerializeField] private UIPopupSetBid uiPopupSetBid;
+
+        public void ShowUIPopupBid()
+        {
+            uiPopupSetBid.Show();
+        }
     }
 }
