983c707518e8cc18d55bc1ecfd31467cb7b2ffa5
yurii <stolbetskiy.yura@gmail.com>
2022-09-24 21:35:19 +0300

    Small fixed

---
+++ Assets/Scripts/UI/Profile/CreateTeamLogoView.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Profile
+{
+    public class CreateTeamLogoView : MonoBehaviour
+    {
+        [SerializeField] private Image firstLayerImg;
+
+        public void ChangeFirstLayer()
+        {
+        }
+    }
+}
--- Assets/UI/Scripts/CardsFilter.cs
+++ Assets/UI/Scripts/CardsFilter.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UI.Scripts.Card;
 using Unity.VisualScripting;
