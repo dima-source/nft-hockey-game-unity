6ae0bcd7c47bc2975dd28754160daa1622a6f4c0
advanced-user <stolbetskiy.yura@gmail.com>
2022-10-02 05:52:36 +0300

    Implemented player role filter

--- Assets/Scripts/Near/Models/Tokens/Filters/ToggleFilters/ToggleFilterRole.cs
+++ Assets/Scripts/Near/Models/Tokens/Filters/ToggleFilters/ToggleFilterRole.cs
@@ -1,4 +1,6 @@
-﻿using UI.Scripts;
+﻿using System;
+using System.Collections.Generic;
+using UI.Scripts;
 
 namespace Near.Models.Tokens.Filters.ToggleFilters
 {
@@ -6,8 +8,58 @@ namespace Near.Models.Tokens.Filters.ToggleFilters
     {
         public void AddToPlayerFilter(PlayerFilter playerFilter, Toggle[] toggles)
         {
-            
+            List<string> playerRoles = new List<string>();
+
+            foreach (Toggle toggle in toggles)
+            {
+                if (!toggle.isOn || toggle.text == "Some toggle")
+                {
+                    continue;
+                }
+
+                string playerRole = TransformPlayerRole(toggle.text);
+                playerRoles.Add(playerRole);
+            }
+
+            if (playerRoles.Count != 0)
+            {
+                playerFilter.player_role_in = playerRoles;
+            }
         }
-        
+
+        private string TransformPlayerRole(string playerRole)
+        {
+            switch (playerRole)
+            {
+                case "Thy-harder":
+                    return "TryHarder";
+                case "Defensive forward":
+                    return "DefensiveForward";
+                case "Defensive defenseman":
+                    return "DefensiveDefenseman";
+                case "Offensive defenseman":
+                    return "OffensiveDefenseman";
+                case "Two-way":
+                    return "TwoWay";
+                case "Tough guy":
+                    return "ToughGuy";
+                case "Stand up":
+                    return "StandUp";
+                case "Playmaker":
+                    return "Playmaker";
+                case "Enforcer":
+                    return "Enforcer";
+                case "Shooter":
+                    return "Shooter";
+                case "Grinder":
+                    return "Grinder";
+                case "Butterfly":
+                    return "Butterfly";
+                case "Hybrid":
+                    return "Hybrid";
+                default:
+                    throw new Exception("Player role not found");
+            }
+        }
     }
 }
