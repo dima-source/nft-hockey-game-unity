530620338a1e3324b6b1759bfc2214250a3e3776
advanced-user <stolbetskiy.yura@gmail.com>
2022-11-17 10:23:49 +0200

    Added script for canvas

---
+++ Assets/Scripts/GameScene/UI/Canvas.cs
@@ -0,0 +1,27 @@
+﻿using UI.Scripts;
+using UnityEngine;
+
+namespace GameScene.UI
+{
+    public class Canvas : UiComponent
+    {
+        protected override void Initialize()
+        {
+            var phoneLayout = global::UI.Scripts.Utils.FindChild<GameView>(transform, "PhoneLayout");
+            var tabletLayout = global::UI.Scripts.Utils.FindChild<GameView>(transform, "TabletLayout");
+
+            var rectTransform = GetComponent<RectTransform>();
+
+            if (rectTransform.rect.width / rectTransform.rect.height >= 2)
+            {
+                phoneLayout.gameObject.SetActive(true);
+                tabletLayout.gameObject.SetActive(false);
+            }
+            else
+            {
+                phoneLayout.gameObject.SetActive(false);
+                tabletLayout.gameObject.SetActive(true);
+            }
+        }
+    }
+}
