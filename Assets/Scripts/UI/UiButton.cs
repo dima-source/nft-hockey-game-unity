5f5a90fd883ebcab150ddbf5f6bd025ea88d22fb
yurii <stolbetskiy.yura@gmail.com>
2023-02-03 16:16:41 +0300

    Removed some sprites. Added rounded sprites. Small fix

--- Assets/Scripts/UI/UiButton.cs
+++ Assets/Scripts/UI/UiButton.cs
@@ -1,6 +1,5 @@
 ﻿using TMPro;
 using UI.Scripts;
-using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
@@ -18,48 +17,12 @@ namespace UI
         
         private TextMeshProUGUI _text;
         private Button _button;
-        private Image _stroke;
         private Image _background;
-
-        private RectTransform _strokeRectTransform;
-        private RectTransform _backgroundRectTransform;
 
         protected override void Initialize()
         {
             _button = GetComponent<Button>();
             _text =  UiUtils.FindChild<TextMeshProUGUI>(transform, "Text");
-            
-            _stroke = GetComponent<Image>();
-            _background = UiUtils.FindChild<Image>(transform, "MainArea");
-            
-            _strokeRectTransform = GetComponent<RectTransform>();
-            _backgroundRectTransform = UiUtils.FindChild<RectTransform>(transform, "MainArea");
-        }
-
-        protected override void OnUpdate()
-        {
-            SetStroke();
-        }
-
-        private void SetStroke()
-        {
-            var sizeDeltaStroke = _strokeRectTransform.sizeDelta;
-            float widthStroke = sizeDeltaStroke.x / 30;
-            
-            _backgroundRectTransform.sizeDelta = new Vector2(sizeDeltaStroke.x - widthStroke,
-               sizeDeltaStroke.y - widthStroke);
-
-            _backgroundRectTransform.anchoredPosition = new Vector2(0, 0);
-        }
-        
-        private void SetBackgroundMaterial(string path)
-        {
-            _background.material = UiUtils.LoadResource<Material>(path);
-        }
-
-        private void SetStrokeMaterial(string path)
-        {
-            _stroke.material = UiUtils.LoadResource<Material>(path);
         }
 
         private void SetText(string text)
