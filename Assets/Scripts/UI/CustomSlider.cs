175a44f1663df92c8e85215e85143220337b2966
advanced-user <stolbetskiy.yura@gmail.com>
2023-03-01 22:06:33 +0200

    Added slider prefab

---
+++ Assets/Scripts/UI/CustomSlider.cs
@@ -0,0 +1,17 @@
+﻿using UI.Scripts;
+using UnityEngine;
+
+namespace UI
+{
+    public class CustomSlider : UiComponent
+    {
+        private Transform _handleSlideArea;
+        private const string UnselectedValuePath = "/Assets/Resources/Prefabs/Slider/UnselectedValueImg";
+        
+        protected override void Initialize()
+        {
+            _handleSlideArea = UiUtils.FindChild<Transform>(transform, "Handle Slide Area");
+            
+        }
+    }
+}
